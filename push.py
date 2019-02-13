1a54bd06d40a7b32556e036e4a9a36ba3d457928
evi1haxor <architdwivedi.off@gmail.com>
2019-02-14 02:11:22 +0530

    M  push.py

--- push.py
+++ push.py
@@ -1,15 +1,44 @@
 import subprocess
 import argparse
 import sys
+
 p = argparse.ArgumentParser()
+
 if len(sys.argv) == 3:
     p.add_argument("stg", help = "specs for stage")
-p.add_argument("commit_message", help = "CommitMessage <message should be in triple dual-quotes.>")
+    p.add_argument("commit_message", help = "CommitMessage <message should be in triple dual-quotes.>")
+elif len(sys.argv) == 2:
+    p.add_argument("commit_message", help = "CommitMessage <message should be in triple dual-quotes.>")
+
+
 a = p.parse_args()
+
+
 subprocess.call("git status", shell = True)
-input()
+
 if len(sys.argv) == 3:
-    subprocess.call("git add -" + a.stg)
+    subprocess.call("git add -" + a.stg, shell = True)
+    t = a.commit_message
 else:
-    subprocess.call("git add .")
-subprocess.call("git commit -m "  + "\"" + a.commit_message + "\"" + " && git push origin master", shell = True)
+    subprocess.call("git add .", shell = True)
+    t = list(str(subprocess.check_output("git status --porcelain", shell = True)))
+    if len(sys.argv) > 1 and a.commit_message != "":
+        t = a.commit_message
+
+
+m = 0
+cmsg = ""
+
+for a in t:
+    if a == '\\':
+        m = 1
+        a = ''
+    elif m ==1:
+        a = '; '
+        m = 0
+    if a == '\'':
+        a = ''
+    cmsg += str(a)
+
+
+subprocess.call("git commit -m " + "\"" + cmsg + "\"" + " && git push origin master", shell = True)
