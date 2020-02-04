de8c4c4491f17952655d81eb24612722b4673f4c
evi1haxor <architdwivedi.off@gmail.com>
2020-02-04 17:34:50 +0530

    M  shscripts/touchpad.sh

--- shscripts/touchpad.sh
+++ shscripts/touchpad.sh
@@ -2,6 +2,7 @@
 
 id=$(xinput list | grep "ETPS/2 Elantech Touchpad" | cut -f2- -d'=' | cut -b 1,2)
 
+ad=$(xinput list-props "$id" | grep "libinput Tapping Enabled (" | cut -f2- -d'(' | cut -b 1,2,3)
 
-#echo "$id"
-xinput set-prop "$id" 334 1
+xinput set-prop "$id" "$ad" 1
+
